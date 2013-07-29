d024cfa016029322797a3cc1d7573d15821bd5df
apoon <apoon@yahoo-inc.com>
2013-07-29 16:42:04 -0700

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TemperatureConverterAppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Aaron Poon on 7/29/13.
+//  Copyright (c) 2013 Aaron Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TemperatureConverterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TemperatureConverter
+//
+//  Created by Aaron Poon on 7/29/13.
+//  Copyright (c) 2013 Aaron Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TemperatureConverterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TemperatureConverterAppDelegate class]));
+    }
+}
